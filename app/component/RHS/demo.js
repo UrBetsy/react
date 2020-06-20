4df99df717d49c3ccae25a2065ccc9c96d64cbbb
lifankeke <lifankeke@foxmail.com>
2020-06-20 19:31:13 +0800

    useReducer

--- app/component/RHS/demo.js
+++ app/component/RHS/demo.js
@@ -1,46 +1,47 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, useReducer, createContext, useContext } from 'react'
 import { destroyFns } from 'antd/lib/modal/Modal'
 const Context = createContext(null)
 const fns = new Set()
 
-const PlusFirst = React.memo(({plus}) => {
-    const api = useContext(Context);
-    return <button onClick={api.plus}>Plus</button>
+const PlusFirst = React.memo(() => {
+    const dispatch = useContext(Context);
+    return <button onClick={ () => dispatch({type: 'plus'}) }>Plus</button>
 })
-const PlusSecond = React.memo(({plus2}) => {
-    const api = useContext(Context);
-    return <button onClick={api.plus2}>Plus2</button>
+const PlusSecond = React.memo(() => {
+    const dispatch = useContext(Context);
+    return <button onClick={() => dispatch({type: 'plus2'})}>Plus2</button>
 })
 
-const Header = React.memo(({plus, plus2}) => {
-    return (
-        <div className='list-header'>
-            <span> options: </span>
-            <PlusFirst plus = {plus}></PlusFirst>
-            <PlusSecond plus2 = {plus2}></PlusSecond>
-        </div> 
-    )
-    
-})
+const Header = () => (
+    <div className='list-header'>
+        <span> options: </span>
+        <PlusFirst ></PlusFirst>
+        <PlusSecond ></PlusSecond>
+    </div> 
+)
 
 const Demo = () => {
-    const [countFrist, setCountFirst] = useState(0);
-    const [countSecond, setCountSecond] = useState(0);
-
-    const plus = () => setCountFirst(countFrist + 1);
-    const plus2 = () => setCountSecond(countSecond + 1);
-
-    fns.add(plus)
-    fns.add(plus2)
+    const initState = {countFirst: 0, countSecond: 0};
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'plus':
+                return {...state, countFirst: state.countFirst + 1};
+            case 'plus2':
+                return {...state, countSecond: state.countSecond + 1};
+            default:
+                return state           
+        }
+    }
+    const [state, dispath] = useReducer(reducer, initState)
 
     return (
         <div>
-            <Context.Provider value={{plus, plus2}}>
+            <Context.Provider value={dispath}>
                 <span>------------</span>
                 <Header />
-                <div>function: {fns.size -2 }</div>
-                <div>count1: {countFrist}</div>
-                <div>count2: {countSecond}</div>
+                <div>function: {fns.size}</div>
+                <div>count1: {state.countFirst}</div>
+                <div>count2: {state.countSecond}</div>
             </Context.Provider>
         </div>
     )
